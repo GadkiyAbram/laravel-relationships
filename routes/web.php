eb7e3ae8e835b954e6ffc354d41561850c5c56ed
GadkiyAbram <aabramovskii@gmail.com>
2019-10-19 11:18:30 +0500

    e45 - done
    Eloquent relationships One To One (hasOne, BelongsTo).
    
    Setting up mysql

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $user = factory(\App\User::class)->create();    //creates a new User;
+
+    $user->phone()->create([
+        'phone' => '222-333-4444',
+    ]);
+});
